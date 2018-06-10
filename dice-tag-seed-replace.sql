3be57d28872114add676b878c8f355608145872a
Michael Mason <mikemason930@gmail.com>
2018-06-10 13:23:06 -0400

    Consolidate queries into single file

--- dice-tag-seed-replace.sql
+++
@@ -1,7 +0,0 @@
-UPDATE phpbb_posts SET
-	post_text=REGEXP_REPLACE(post_text, "<!--(\\d+)-->", "SEEDSTART\1SEEDEND");
-WHERE LOCATE("<!--", post_text) != 0;
-
-UPDATE phpbb_privmsgs SET
-	message_text=REGEXP_REPLACE(message_text, "<!--(\\d+)-->", "SEEDSTART\1SEEDEND");
-WHERE LOCATE("<!--", message_text) != 0;
