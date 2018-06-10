d34cbbf811cdc2dbd4876862838440345c1725f9
Michael Mason <mikemason930@gmail.com>
2018-06-10 14:18:23 -0400

    Fix SQL error on dice updates

--- migration/db/data/before.sql
+++ migration/db/data/before.sql
@@ -97,9 +97,9 @@ INSERT INTO `phpbb_bbcodes` VALUES
 ###
 
 UPDATE phpbb_posts SET
-	post_text=REGEXP_REPLACE(post_text, "<!--(\\d+)-->", "SEEDSTART\1SEEDEND");
+	post_text=REGEXP_REPLACE(post_text, "<!--(\\d+)-->", "SEEDSTART\1SEEDEND")
 WHERE LOCATE("<!--", post_text) != 0;
 
 UPDATE phpbb_privmsgs SET
-	message_text=REGEXP_REPLACE(message_text, "<!--(\\d+)-->", "SEEDSTART\1SEEDEND");
+	message_text=REGEXP_REPLACE(message_text, "<!--(\\d+)-->", "SEEDSTART\1SEEDEND")
 WHERE LOCATE("<!--", message_text) != 0;
