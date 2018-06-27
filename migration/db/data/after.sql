3872032497d8a7558e03903256e4bc2d1bf830ad
Michael Mason <mikemason930@gmail.com>
2018-06-27 14:36:29 -0400

    Set sepia style ID

--- migration/db/data/after.sql
+++ migration/db/data/after.sql
@@ -11,10 +11,14 @@ SELECT @BLACK_ID:=style_id
 FROM phpbb_styles
 WHERE style_name='mafBlack';
 
+SELECT @SEPIA_ID:=style_id
+FROM phpbb_styles
+WHERE style_name='mafSepia';
+
 UPDATE phpbb_users, temp_user_old_style SET
 	phpbb_users.user_style=
 		IF(temp_user_old_style.style_id = 4, @SILVER_ID, #If old style is mafSilver
-		IF(temp_user_old_style.style_id = 6, @BLACK_ID, #if old style is mafSepia
+		IF(temp_user_old_style.style_id = 6, @SEPIA_ID, #if old style is mafSepia
 		@BLACK_ID ##Every other style(scuMobile, mafTigers, prosilver, mafMobile2, quilDark, mafBlack)
 	))
 WHERE phpbb_users.user_id=temp_user_old_style.user_id;
