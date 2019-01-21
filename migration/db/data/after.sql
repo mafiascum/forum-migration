cb8e4a1f5404c19d9bbeb63557ad26a19334d4b5
Michael Mason <mikemason930@gmail.com>
2019-01-20 21:01:45 -0800

    Set up team page + setup instructions

--- migration/db/data/after.sql
+++ migration/db/data/after.sql
@@ -24,3 +24,58 @@ UPDATE phpbb_users, temp_user_old_style SET
 WHERE phpbb_users.user_id=temp_user_old_style.user_id;
 
 DROP TABLE IF EXISTS `temp_user_old_style`;
+
+###
+#
+# Move custom profile contact fields to new table.
+#
+###
+UPDATE `phpbb_profile_fields_data`, `phpbb_users` SET
+	`phpbb_profile_fields_data`.`pf_phpbb_skype`=`phpbb_users`.`user_skype`,
+	`phpbb_profile_fields_data`.`pf_phpbb_facebook`=`phpbb_users`.`user_facebook`,
+	`phpbb_profile_fields_data`.`pf_phpbb_twitter`=`phpbb_users`.`user_twitter`
+WHERE `phpbb_profile_fields_data`.`user_id`=`phpbb_users`.`user_id`;
+
+# regexp_replace(regexp_replace("http://www.twitter.com/wchare", "^(?:https?://)(?:www\\.)twitter\\.com/(.*?)/?", ""), "/", "");
+# regexp_replace(regexp_replace("https://www.facebook.com/will.hare.89/", "^(?:https?://)(?:www\\.)facebook\\.com/(.*?)/?", ""), "/", "");
+
+# 
+
+INSERT INTO `phpbb_profile_fields_data`
+SELECT
+	`phpbb_users`.`user_id`,
+	null,
+	null,
+	'',
+	'',
+	`phpbb_users`.`user_facebook`,
+	'',
+	`phpbb_users`.`user_skype`,
+	`phpbb_users`.`user_twitter`,
+	'',
+	'',
+	'',
+	''
+FROM `phpbb_users`
+LEFT JOIN `phpbb_users` ON (`phpbb_profile_fields_data`.`user_id` = `phpbb_users`.`user_id`)
+WHERE `phpbb_profile_fields_data`.`user_id` IS NULL;
+
+ALTER TABLE `phpbb_users`
+DROP `user_skype`,
+DROP `user_facebook`,
+DROP `user_twitter`;
+
+###
+#
+# Set up the new team page
+#
+###
+TRUNCATE TABLE phpbb_teampage;
+
+INSERT INTO phpbb_teampage VALUES
+(1,13637,'',1,0),
+(3,4,'',2,0),
+(4,13709,'',3,0),
+(5,13667,'',4,0),
+(6,13647,'',5,0),
+(7,13696,'',6,0);
